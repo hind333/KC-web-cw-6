514c18e0b1cecc9858150f6977b5e2a37b89059f
hind333 <109863744+hind333@users.noreply.github.com>
2022-08-13 14:00:09 +0300

    وضعت ما طُلب مع القيل من الأختصارات

---
+++ script.js
@@ -0,0 +1,14 @@
+alert(`مرحباً بك في موقعي الذي سيحسب درجتك!!`)
+let grade = prompt(`اكتب درجتك بالأرقام`)
+console.log(grade)
+if(grade >=90 && grade >= 100 ){
+    console.log(`لقد حصلت على امتياز 🥳`)
+}else if(grade>= 80  && grade>= 89){
+    console.log(` جيد جدا`)
+}else if (grade>=70 && grade>=79 ){
+    console.log (`جيد`)
+}else if(grade>=60 && grade>=69){
+    console.log(`لقد حصلت على مقبول`)
+}else if(grade>=50 && grade>=59){
+    console.log(`ضعيف`)
+}else console.log(`ضعيف`)
